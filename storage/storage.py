2443ed6a069518b288e158e445355c4534c249d0
Joaquin Segui <joaquin.segui@mercadolibre.com>
2019-12-02 21:54:43 -0300

    Adds storage basic behaviour

--- storage/storage.py
+++ storage/storage.py
@@ -3,26 +3,64 @@
 import os
 import logging
 from multiprocessing import Process
-from storage_master import StorageMaster
-from storage_replica import StorageReplica
+# from storage_master import StorageMaster
+# from storage_slave import StorageSlave
 from rabbitmq_queue import RabbitMQQueue
+
+MASTER_NEEDED_MSG = 'M'
+SLAVE_ROLE  = 'slave'
+MASTER_ROLE = 'master'
+READ_COMMAND = 'CATCHUP'
 
 class Storage:
     def __init__(self, pid):
-        self.role = StorageReplica(pid)
-
+        self.pid = pid
+        self.role = SLAVE_ROLE
+        self.input_queue = RabbitMQQueue(exchange='slave', consumer=True, exclusive=True, queue_name='slave{}_queue'.format(pid))
+        self.slaves = RabbitMQQueue(exchange='slave')
+        self.master_queue = RabbitMQQueue(exchange = 'storage_input', consumer = True, queue_name = 'master_queue')
+        self.output_queue = RabbitMQQueue(exchange = 'storage_output')                            
+        
     def run(self):
-        self.role.run()
+        self.input_queue.consume(self.process)
 
-    def change_role(self, ch, method, properties, body):
-        logging.info('Received %r' % body)
-        role = body.decode()
-        if role == 'master':
-            logging.info('Changing to master role')
-            self.role = StorageMaster()
+    def process(self, ch, method, properties, body):
+        if self.role == SLAVE_ROLE:
+            logging.info('[SLAVE] Received %r' % body)
+            msg = body.decode()
+            if msg == MASTER_NEEDED_MSG:
+                logging.info('[SLAVE] I was asked to be the new Storage Master')
+                self.role = MASTER_ROLE
+                # TODO: ACK message
+                self.master_queue.consume(self.processMaster)
+            logging.info('[SLAVE] Saving message')
+            self.persistState(body)
+            # TODO: ACK message
+
+
+    def processMaster(self, ch, method, properties, body):
+        logging.info('[MASTER] Received %r' % body)
+        if self.isReadRequest(body):
+            self.processRead(body)
         else:
-            logging.error('Role %s does not exist' % role)
-            return
+            self.persistState(body)
+            self.slaves.publish(body)
+            # TODO: ACK msg
+
+    def persistState(self, body):
+        # TODO: Save state to disk
+        logging.info('Persisting to disk %r' % body)
+
+    def isReadRequest(self, msg):
+        return False
+        # return READ_COMMAND in msg
+
+    def processRead(self, msg):
+        # TODO: parse msg
+        # TODO: fetch state from disk
+        # TODO: build response
+        response = '<STATE>'
+        self.output_queue.publish(response)
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(message)s',
